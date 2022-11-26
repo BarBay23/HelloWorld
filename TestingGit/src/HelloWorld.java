1faec4b775860e52fdc69b7266e75110357bf406
Baran_Baygin <baran.baygin@hotmail.de>
2022-11-26 18:39:29 +0100

    Neue nice funktion

--- TestingGit/src/HelloWorld.java
+++ TestingGit/src/HelloWorld.java
@@ -1,7 +1,11 @@
 
 public class HelloWorld {
+	
+	private static void nice() {
+		System.out.println("nice");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		nice();
 	}
 }
